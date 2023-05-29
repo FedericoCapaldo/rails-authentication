c7575f78298fe1d18f7fda194cefe09d15fd4b20
Federico <federico.capaldo@hotmail.com>
2023-05-29 10:42:08 +0100

    adde views for devise and added first_name and last_name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+    if user_signed_in?
+      @my_user = current_user.first_name
+    end
+  end
+
+  def about
   end
 end
